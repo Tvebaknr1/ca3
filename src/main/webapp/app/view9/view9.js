7cac65765ef643687f1d284becbf61478be5006b
Louise B. Nielsen <Louise B. Nielsen>
2016-11-01 12:54:27 +0100

    create user from view9

--- src/main/webapp/app/view9/view9.js
+++ src/main/webapp/app/view9/view9.js
@@ -5,4 +5,10 @@ angular.module('myApp.view9', ['ngRoute'])
               templateUrl: 'app/view9/view9.html',
               controller: 'View9Ctrl'
             });
+          }]).controller('View9Ctrl', ['$scope', function($scope){
+          $scope.add = function(username, password){
+              console.log("" + username + password);
+              var user = {username: '', password: ''};
+              
+          };
           }]);
