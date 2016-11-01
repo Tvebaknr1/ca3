c83abf05e922d78679df0d6652d56e3ce0c2ed8b
Louise B. Nielsen <Louise B. Nielsen>
2016-11-01 12:28:05 +0100

    views

--- src/main/webapp/app/view8/view8.js
+++ src/main/webapp/app/view8/view8.js
@@ -5,14 +5,4 @@ angular.module('myApp.view8', ['ngRoute'])
                     templateUrl: 'app/view8/view8.html',
                     controller: 'View8Ctrl'
                 });
-            }])
-
-        .controller('View8Ctrl', function ($http, $scope) {
-            $http.get('api/demoadmin')
-                    .success(function (data, status, headers, config) {
-                        $scope.data = data;
-                    })
-                    .error(function (data, status, headers, config) {
-
-                    });
-        });
+            }]);
--- src/main/webapp/app/view9/view9.js
+++ src/main/webapp/app/view9/view9.js
@@ -5,16 +5,4 @@ angular.module('myApp.view9', ['ngRoute'])
               templateUrl: 'app/view9/view9.html',
               controller: 'View9Ctrl'
             });
-          }])
-
-        .controller('View9Ctrl', function ($http, $scope) {
-          $http({
-            method: 'GET',
-            url: 'api/demouser'
-          }).then(function successCallback(res) {
-            $scope.data = res.data.message;
-          }, function errorCallback(res) {
-            $scope.error = res.status + ": "+ res.data.statusText;
-          });
-
-        });
+          }]);
