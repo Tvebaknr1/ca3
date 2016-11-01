3685222fdbe80d670124d7b16aed2cf1eb808f4e
Lange-Laptop-Pc\Lange-Laptop <peterleolange@yahoo.dk>
2016-11-01 12:02:43 +0100

    pushing it to the limit

--- src/main/webapp/app/view8/view8.js
+++ src/main/webapp/app/view8/view8.js
@@ -1,20 +1,18 @@
 angular.module('myApp.view8', ['ngRoute'])
 
         .config(['$routeProvider', function ($routeProvider) {
-            $routeProvider.when('/view8', {
-              templateUrl: 'app/view8/view8.html',
-              controller: 'View8Ctrl'
-            });
-          }])
+                $routeProvider.when('/view8', {
+                    templateUrl: 'app/view8/view8.html',
+                    controller: 'View8Ctrl'
+                });
+            }])
 
         .controller('View8Ctrl', function ($http, $scope) {
-          $http({
-            method: 'GET',
-            url: 'api/demouser'
-          }).then(function successCallback(res) {
-            $scope.data = res.data.message;
-          }, function errorCallback(res) {
-            $scope.error = res.status + ": "+ res.data.statusText;
-          });
+            $http.get('api/demoadmin')
+                    .success(function (data, status, headers, config) {
+                        $scope.data = data;
+                    })
+                    .error(function (data, status, headers, config) {
 
+                    });
         });
