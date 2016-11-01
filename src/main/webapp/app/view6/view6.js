756e8df3cb0e15ca3eb4761bdb5301ffa4a91f1a
Lange-Laptop-Pc\Lange-Laptop <peterleolange@yahoo.dk>
2016-11-01 11:45:41 +0100

    auth for app6 og 7

--- src/main/webapp/app/view6/view6.js
+++ src/main/webapp/app/view6/view6.js
@@ -1,7 +1,20 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+angular.module('myApp.view6', ['ngRoute'])
 
+        .config(['$routeProvider', function ($routeProvider) {
+            $routeProvider.when('/view6', {
+              templateUrl: 'app/view6/view6.html',
+              controller: 'View6Ctrl'
+            });
+          }])
 
+        .controller('View6Ctrl', function ($http, $scope) {
+          $http({
+            method: 'GET',
+            url: 'api/demouser'
+          }).then(function successCallback(res) {
+            $scope.data = res.data.message;
+          }, function errorCallback(res) {
+            $scope.error = res.status + ": "+ res.data.statusText;
+          });
+
+        });
--- src/main/webapp/app/view7/view7.js
+++ src/main/webapp/app/view7/view7.js
@@ -1,7 +1,20 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+angular.module('myApp.view7', ['ngRoute'])
 
+        .config(['$routeProvider', function ($routeProvider) {
+            $routeProvider.when('/view7', {
+              templateUrl: 'app/view7/view7.html',
+              controller: 'View7Ctrl'
+            });
+          }])
 
+        .controller('View7Ctrl', function ($http, $scope) {
+          $http({
+            method: 'GET',
+            url: 'api/demouser'
+          }).then(function successCallback(res) {
+            $scope.data = res.data.message;
+          }, function errorCallback(res) {
+            $scope.error = res.status + ": "+ res.data.statusText;
+          });
+
+        });
