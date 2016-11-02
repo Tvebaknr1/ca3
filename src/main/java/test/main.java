087aef5030d634b7bb4cca6ac033a1504093dc81
EmilUlrikChristoffersen <emilulrikchristoffersen@outlook.dk>
2016-11-02 11:57:15 +0100

    user-agent

--- src/main/java/test/main.java
+++ src/main/java/test/main.java
@@ -26,7 +26,7 @@ public class main {
     private static final Map<String, IUser> users = new HashMap<>();
     private static String salt = "salt";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws PasswordStorage.CannotPerformOperationException {
         HashMap<String, Object> puproperties = new HashMap();
 
 //        puproperties.put("javax.persistence.sql-load-script-source", "scripts/ClearDB.sql");
@@ -34,32 +34,33 @@ public class main {
         Persistence.generateSchema("ca3", null);
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("ca3");
         EntityManager em = emf.createEntityManager();
-        User user = new User("user", "test");
+        User user = new User("user", PasswordStorage.createHash("test"+salt));
         user.addRole("User");
         em.getTransaction().begin();
         em.flush();
         em.persist(user);
         em.getTransaction().commit();
         em.close();
-
+        System.out.println("test");
         users.put(user.getUserName(), user);
         em = emf.createEntityManager();
-        User admin = new User("admin", "test");
+        User admin = new User("admin", PasswordStorage.createHash("test"+salt));
         admin.addRole("Admin");
         em.getTransaction().begin();
         em.persist(admin);
         em.getTransaction().commit();
         em.close();
-
+        System.out.println("test2");
         users.put(admin.getUserName(), admin);
         em = emf.createEntityManager();
-        User both = new User("user_admin", "test");
+        User both = new User("user_admin", PasswordStorage.createHash("test"+salt));
         both.addRole("User");
         both.addRole("Admin");
         em.getTransaction().begin();
         em.persist(both);
         em.getTransaction().commit();
         em.close();
+        System.out.println("test3");
 
         users.put(both.getUserName(), both);
     }
--- src/main/webapp/app/view6/view6.js
+++ src/main/webapp/app/view6/view6.js
@@ -16,7 +16,23 @@ angular.module('myApp.view6', ['ngRoute'])
         .factory('userService', function ($http) {
             return {
                 getCompanies: function () {
-                    return $http.get("http://cvrapi.dk/api?vat=3167%208021&country=dk");
+                    var req = {
+                        method: 'GET',
+                        url: 'http://cvrapi.dk/api?vat=3167%208021&country=dk',
+                        headers: {
+                            'User-Agent': "CVR API-CA3 SCHOOL Exercise-Gruppe 6, cph-ec57@cphbusiness.dk"
+                        },
+                        data: {}
+                    };
+
+                     return $http(req);
+//                             .then(function mySucces (response) {
+//                         response.data;
+//                    }, function myError (response) {
+//                        return response.statusText;
+//                    });
+//                    $http.defaults.headers.common.Authorization = "CVR API-CA3 SCHOOL Exercise-YOUR NAMEYOUREMAIL@cphbusiness.dk";
+//                            return $http.get("http://cvrapi.dk/api?vat=3167%208021&country=dk");
                 }
             };
         });
