3cf7e896a5857985c4fe3af9983ef7208fbc01dd
EmilUlrikChristoffersen <emilulrikchristoffersen@outlook.dk>
2016-11-03 12:35:15 +0100

    daylyupdate

--- src/main/java/test/BackgroundJobManager.java
+++ src/main/java/test/BackgroundJobManager.java
@@ -29,28 +29,24 @@ import org.xml.sax.helpers.XMLReaderFactory;
 @Singleton
 public class BackgroundJobManager {
 
-    
     public static void main(String[] args) {
-        Updatecurrentcy();
-        ArrayList<Currency> array;
-        array = XmlReaderDemo.getoutput();
-        System.out.println(array);
-        for (Currency currency : array) {
-            currencyFacade.createcurrency(currency);
-        }
-        
+
     }
+
     @Schedule(hour = "0", minute = "0", second = "0", persistent = false)
-    public static XMLReader Updatecurrentcy() {
+    public static void Updatecurrentcy() {
         try {
             XMLReader xr = XMLReaderFactory.createXMLReader();
             xr.setContentHandler(new XmlReaderDemo());
             URL url = new URL("http://www.nationalbanken.dk/_vti_bin/DN/DataService.svc/CurrencyRatesXML?lang=en");
             xr.parse(new InputSource(url.openStream()));
-            return xr;
+            ArrayList<Currency> array;
+            array = XmlReaderDemo.getoutput();
+            for (Currency currency : array) {
+                currencyFacade.createcurrency(currency);
+            }
         } catch (SAXException | IOException ex) {
             Logger.getLogger(BackgroundJobManager.class.getName()).log(Level.SEVERE, null, ex);
         }
-        return null;
     }
 }
