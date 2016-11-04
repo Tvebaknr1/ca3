75d0b55532f762b2fb8c9c29f8280c9bf8e776f9
Emilie <ehn1994@gmail.com>
2016-11-04 11:22:12 +0100

    asd

--- src/main/webapp/app/view5/view5.js
+++ src/main/webapp/app/view5/view5.js
@@ -3,9 +3,7 @@ angular.module('myApp.view5', ['ngRoute'])
         .config(['$routeProvider', function ($routeProvider) {
                 $routeProvider.when('/view5', {
                     templateUrl: 'app/view5/view5.html',
-                    controller: 'View5Ctrl'
+                   
                 });
-            }]).controller("View5Ctrl", function(){
-            
-            });
+            }]);
             
